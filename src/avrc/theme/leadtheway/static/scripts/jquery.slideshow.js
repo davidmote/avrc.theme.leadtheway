c915cf0dec28c28b312d34697580029cd95f44e8
David Mote <davidmote@gmail.com>
2011-05-03 20:34:47 +0000

    updated css post launch

--- src/avrc/theme/leadtheway/static/scripts/jquery.slideshow.js
+++ src/avrc/theme/leadtheway/static/scripts/jquery.slideshow.js
@@ -2,7 +2,7 @@
 	$(window).load(
 		    function(){ //$(window).load() must be used instead of $(document).ready() because of Webkit compatibility
 		        $("#slideshow").sliderkit({
-		                autospeed:3000,
+		                autospeed:30000,
 		                panelbtnshover:false,
 		                circular:true
 		        });
