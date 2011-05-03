3843d24b01b3f3854e66fa66bd18ba9064b866a4
David Mote <davidmote@gmail.com>
2011-05-03 20:47:35 +0000

    added randomization to top images

--- src/avrc/theme/leadtheway/browser/viewlets.py
+++ src/avrc/theme/leadtheway/browser/viewlets.py
@@ -3,7 +3,7 @@ from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
 from plone.app.layout.viewlets.interfaces import IPortalHeader
 from zope.interface import Interface
 from zope.app.component.hooks import getSite
-
+import random
 # Apply on everything.
 # Use templates directory to search for templates.
 grok.templatedir('templates')
@@ -25,7 +25,8 @@ class SlideshowViewlet(grok.Viewlet):
     def slideshowImages(self):
         slideshow_container = self.slideshowContainer()
         if slideshow_container is not None:
-            imageBrains = slideshow_container.getFolderContents({'portal_type':'Image'})
+            imageBrains = list(slideshow_container.getFolderContents({'portal_type':'Image'}))
+            random.shuffle(imageBrains)
         else:
             imageBrains = []
         return imageBrains
