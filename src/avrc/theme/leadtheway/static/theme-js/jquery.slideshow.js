cc7b4ba49f7be5ab71c500c921a77ec47f7ae958
David Mote <davidmote@gmail.com>
2011-10-06 16:35:04 -0700

    fixes from testing on productions database

--- src/avrc/theme/leadtheway/static/theme-js/jquery.slideshow.js
+++ src/avrc/theme/leadtheway/static/theme-js/jquery.slideshow.js
@@ -1,11 +1,9 @@
 (function($){
-	$(window).load(
-		    function(){ //$(window).load() must be used instead of $(document).ready() because of Webkit compatibility
-		        $("#slideshow").sliderkit({
-		                autospeed:10000,
-		                panelbtnshover:false,
-		                circular:true
-		        });
-
-		);
+    $(window).load(function(){ //$(window).load() must be used instead of $(document).ready() because of Webkit compatibility
+            $("#slideshow").sliderkit({
+                autospeed:10000,
+                panelbtnshover:false,
+                circular:true
+            });
+    });
 })(jQuery)
