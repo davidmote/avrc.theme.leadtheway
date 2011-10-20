bbfcb8bc22781a5721bfbf2e59ec2e75e4f50abc
David Mote <damote@ucsd.edu>
2011-10-20 15:27:36 -0700

    add piwik to the bottom of the template

--- src/avrc/theme/leadtheway/static/theme-js/jquery.slideshow.js
+++ src/avrc/theme/leadtheway/static/theme-js/jquery.slideshow.js
@@ -1,7 +1,7 @@
 (function($){
     $(window).load(function(){ //$(window).load() must be used instead of $(document).ready() because of Webkit compatibility
         $("#slideshow").sliderkit({
-            autospeed:10000,
+            autospeed:5000,
             panelbtnshover:false,
             circular:true
         });
